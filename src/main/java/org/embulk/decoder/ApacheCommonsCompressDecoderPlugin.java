216e9a288322f519813854627beef0c9cf55223c
Hiroyuki Sato <hiroysato@gmail.com>
2015-02-23 21:18:15 +0900

    Implement basic bzip2 decoder

--- src/main/java/org/embulk/decoder/ApacheCommonsCompressDecoderPlugin.java
+++ src/main/java/org/embulk/decoder/ApacheCommonsCompressDecoderPlugin.java
@@ -1,12 +1,24 @@
 package org.embulk.decoder;
 
+import java.io.InputStream;
+import java.io.IOException;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.config.ConfigSource;
+import org.embulk.config.ConfigInject;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
 import org.embulk.spi.DecoderPlugin;
 import org.embulk.spi.FileInput;
+
+import org.embulk.spi.BufferAllocator;
+
+import org.embulk.spi.util.FileInputInputStream;
+import org.embulk.spi.util.InputStreamFileInput;
+
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.xz.XZCompressorInputStream;
+import org.apache.commons.compress.compressors.z.ZCompressorInputStream;
 
 public class ApacheCommonsCompressDecoderPlugin
         implements DecoderPlugin
@@ -14,12 +26,16 @@ public class ApacheCommonsCompressDecoderPlugin
     public interface PluginTask
             extends Task
     {
-        @Config("property1")
-        public String getProperty1();
 
-        @Config("property2")
-        @ConfigDefault("0")
-        public int getProperty2();
+        @ConfigInject
+        public BufferAllocator getBufferAllocator();
+
+//        @Config("property1")
+//        public String getProperty1();
+
+//        @Config("property2")
+//        @ConfigDefault("0")
+//        public int getProperty2();
     }
 
     @Override
@@ -34,7 +50,23 @@ public class ApacheCommonsCompressDecoderPlugin
     public FileInput open(TaskSource taskSource, FileInput input)
     {
         PluginTask task = taskSource.loadTask(PluginTask.class);
+        final FileInputInputStream files = new FileInputInputStream(input);
+        return new InputStreamFileInput(
+                task.getBufferAllocator(),
+                new InputStreamFileInput.Provider() {
+                    public InputStream openNext() throws IOException
+                    {
+                        if (!files.nextFile()) {
+                            return null;
+                        }
+//                        return new GZIPInputStream(files);
+                          return new BZip2CompressorInputStream(files);
+                    }
 
-        // TODO
+                    public void close() throws IOException
+                    {
+                        files.close();
+                    }
+                });
     }
 }
